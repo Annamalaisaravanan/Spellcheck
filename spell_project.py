00b5a94d4d3bf34a772041b4354e647bd4326737
Annamalaisaravanan <49229516+Annamalaisaravanan@users.noreply.github.com>
2021-03-05 07:56:25 +0530

    Add files via upload
    This is a Mini project in python. Spell-checker is about checking the spelling of the city names and correcting it. I used a fuzzy-wuzzy library which is helpful in finding the similarities between words, say Hyderabad and hyderbad will have a similarity of 95%. I have created a frequency list of 40,000 city names, whenever the city name was entered it was checked for the spelling and it go for correction. The word in frequency list which has more similarity with the word entered will be replaced in the sentence. I also setup the model in a way that it rejects the abbreviations and the stop words.

---
+++ spell_project.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from spellchecker import SpellChecker
+import operator
+spell=SpellChecker()
+spell.word_frequency.load_text_file('C:/Users/Annamalai/Downloads/city.txt')
+code_data=pd.read_csv("C:/Users/Annamalai/Downloads/active_airport_new.csv")
+faa_data=code_data["faa"].unique()
+country_code=code_data["CountryCode"].unique()
+user_in=input()
+print(user_in)
+
+lis=user_in.split(" ")
+
+for i,j in enumerate(lis):
+    if j in faa_data:
+        num_data={"airport_name":str(list(set(code_data.loc[code_data["faa"]==j,"name"]))),"city":str(list(set(code_data.loc[code_data["faa"]==j,"city"]))),"StateCode":str(list(set(code_data.loc[code_data["faa"]==j,"StateCode"]))),"CountryCode":str(list(set(code_data.loc[code_data["faa"]==j,"CountryCode"]))),"CountryName":str(list(set(code_data.loc[code_data["faa"]==j,"CountryName"])))}
+        lis[i]=num_data["airport_name"]
+    elif j in country_code:
+        num_data={"airport_name":str(list(set(code_data.loc[code_data["CountryCode"]==j,"name"]))),"city":str(list(set(code_data.loc[code_data["CountryCode"]==j,"city"]))),"StateCode":str(list(set(code_data.loc[code_data["CountryCode"]==j,"StateCode"]))),"faa":str(list(set(code_data.loc[code_data["CountryCode"]==j,"faa"]))),"CountryName":str(list(set(code_data.loc[code_data["CountryCode"]==j,"CountryName"])))}
+        lis[i]=num_data["CountryName"]
+    else:
+          
+        o=process.extract(query=j,choices=h['city'].values.tolist(),limit=7)
+        pair=dict()
+        pair.clear()
+        for i in o:
+            l=fuzz.ratio(i[0].lower(),j.lower())
+            
+            if l>50:
+                
+                
+                pair[i[0]]=int(l)
+            else:
+                pass
+        sorted_list=dict(sorted(pair.items(),key=operator.itemgetter(1),reverse=True))
+        sorted_list=list(sorted_list.items())
+        lis[i]=sorted_list[0][0]
+print(" ".join(lis))
+
